20dfa1aaf6e838b87fed4dd349323215f72c4f84
Nicolas606 <castillonicolas606@gmail.com>
2021-09-20 08:32:29 -0500

    handler for 404 errors that returns a JSON-formatted 404 status code response.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """  """
 from os import environ
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,12 +11,19 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close():
-    """  """
+    """ call store.close """
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error """
+    return jsonify({'error': "Not found"}), 404
+
+
+
 if __name__ == '__main__':
-    """  """
+    """ Main """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
