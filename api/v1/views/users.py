c405d2dce06fffd91e6a3ac6b44cf3671fe4c3aa
Nicolas606 <castillonicolas606@gmail.com>
2021-09-20 22:16:48 -0500

    Change the POST and PUT

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy.sql.elements import Null
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.user import User
 
@@ -24,7 +24,7 @@ def users_id(user_id):
     all_users_id = storage.get(User, user_id)
     if all_users_id == Null:
         abort(404)
-    return jsonify(all_users_id)
+    return jsonify(all_users_id.to_dict())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
@@ -44,8 +44,31 @@ def delete_user(user_id):
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_users():
     """ Creates a State object"""
+    json = request.get_json()
+    if not json:
+        abort(400, description="Not a JSON")
+    if 'email' not in json:
+        abort(400, description="Missing email")
+    if 'password' not in json:
+        abort(400, description="Missing password")
+
+    user_object = User(**json)
+    user_object.save()
+    return jsonify(user_object.to_dict()), 201
 
 
 @app_views.route('users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_users(user_id):
     """ Updates a State object"""
+    users_id = storage.get(User, user_id)
+    if users_id is None:
+        abort(404)
+
+    json = request.get_json()
+    if json is None:
+        abort(400, description="Not a JSON")
+    for key, value in json.items():
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(users_id, key, value)
+    storage.save()
+    return jsonify(users_id.to_dict()), 200
